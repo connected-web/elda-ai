1a11c185a48adee78512fcdec39934ddc74995e6
John Beech <github@mkv25.net>
2015-10-01 21:50:39 +0100

    Add basic promise support

--- test/api-spec.js
+++ test/api-spec.js
@@ -1,7 +1,7 @@
-var expect = require("chai").expect;
-var elda = require("../lib/api.js");
+var expect = require('chai').expect;
+var elda = require('../lib/api.js');
 
-describe("API", function() {
+describe('API', function() {
 
   var instance;
 
@@ -9,8 +9,26 @@ describe("API", function() {
     instance = elda();
   });
 
-  it("should expose a natural language input", function(done) {
-    expect(typeof instance.respondTo).to.equal('function');
-    done();
+  describe('respondTo', function() {
+
+    it('should be exposed as a function', function(done) {
+      expect(typeof instance.respondTo).to.equal('function');
+      done();
+    });
+
+    it('should return a promise', function(done) {
+      var actual = instance.respondTo();
+      expect(typeof actual.then).to.equal('function');
+      done();
+    });
+
+    it('should reject empty or null messages with an exception', function(done) {
+      instance.respondTo(null).then(function() {
+        done('Unexpected success');
+      }, function(ex) {
+        expect(ex).to.equal('No message provided');
+        done();
+      });
+    });
   });
 });
