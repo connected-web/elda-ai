f3be319632c2206b5c55afa84343a48bd8be6a17
John Beech <github@mkv25.net>
2015-10-05 01:26:47 +0100

    Add checkout commands for repos; test fails on first run

---
+++ test/memory/create-spec.js
@@ -0,0 +1,26 @@
+var expect = require('chai').expect;
+var create = require('../../lib/memory/create');
+var consider = require('../helpers/consider');
+var fs = require('fs');
+
+describe('Create Memory', function() {
+
+  var tempDir = process.cwd() + '/temp';
+  var testConfig = {
+    memory: {
+      local: tempDir,
+      remote: 'git@github.com:connected-web/remote-test.git'
+    }
+  };
+
+  it('retreive files from a remote location', function(done) {
+    create(testConfig).then(function(actual) {
+      consider(function() {
+        var testFile = JSON.parse(fs.readFileSync('./temp/info.json', 'utf-8'));
+        expect(testFile).to.have.property('file', 'info.json');
+      }, done);
+    }, function(ex) {
+      done(ex);
+    });
+  });
+});
