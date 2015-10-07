deae086ac3a9d6f74c32b62300e131e6792abb9c
John Beech <github@mkv25.net>
2015-10-07 02:14:13 +0100

    Implement custom logging on lib/memory/git.js

--- test/memory/memory-config.js
+++ test/memory/memory-config.js
@@ -7,5 +7,11 @@ module.exports = {
   user: {
     name: 'Elda Test',
     email: 'elda-ai@mkv25.net'
+  },
+  log: function() {
+    // Implement tabbed console log to merge inline with test description
+    var args = [].slice.call(arguments);
+    args.unshift('    *');
+    console.log.apply(this, args);
   }
 };
